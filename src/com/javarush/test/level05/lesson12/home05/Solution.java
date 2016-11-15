f68ca70de44e3c24ab424727307310189b448fc2
Tarasov <ol.tarasow@gmail.com>
2016-11-15 16:37:39 +0200

    add level 4 lesson 16 home 04

---
+++ src/com/javarush/test/level05/lesson12/home05/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.test.level05.lesson12.home05;
+
+/* Вводить с клавиатуры числа и считать их сумму
+Вводить с клавиатуры числа и считать их сумму, пока пользователь не введёт слово «сумма». Вывести на экран полученную сумму.
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number = 0;
+        String s = reader.readLine();
+        while (!(s.equals("сумма"))){
+
+            number = number + Integer.parseInt(s);
+            s = reader.readLine();
+        }
+        System.out.print(number);
+    }
+}
